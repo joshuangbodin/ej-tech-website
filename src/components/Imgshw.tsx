0ece2072d26e4079376a40d0012f2600b06ecab4
joshua <ngbodinjoshua@gmail.com>
2024-06-01 17:14:37 +0100

    video adding

--- src/components/Imgshw.tsx
+++ src/components/Imgshw.tsx
@@ -6,7 +6,7 @@ const Imgshw:React.FC = ({}: Props) => {
   return (
     <section className="w-full relative mt-40 md:h-[700px]">
       <span className="block w-full h-1/4 bg-gradient-to-b  z-50 from-white to-transparent absolute "></span>
-      <video src="src\assets\ejtech.mp4" autoPlay loop muted className="imgshw"  />
+      <video src="ejtech.mp4" autoPlay loop muted className="imgshw"  />
 
       <span className="block w-full h-1/4 bg-gradient-to-t z-50 from-white to-transparent top-3/4 absolute "></span>
     </section>
